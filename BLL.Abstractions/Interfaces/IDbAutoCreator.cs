bbea4bde7b77e7b6eec4bd2b0e752061061d93b4
MyLittleSnus <tkachenkomichael09@gmail.com>
2022-01-26 22:09:46 +0200

    Auto creator of db was added

---
+++ BLL.Abstractions/Interfaces/IDbAutoCreator.cs
@@ -0,0 +1,7 @@
+﻿namespace BLL.Abstractions.Interfaces
+{
+    public interface IDbAutoCreator
+    {
+        public void GenerateDb();
+    }
+}
---
+++ BLL/Services/DbAutoCreator.cs
@@ -0,0 +1,46 @@
+﻿using System.Reflection;
+using System;
+using System.IO;
+using System.Linq;
+using BLL.Abstractions.Interfaces;
+
+namespace BLL.Services
+{
+    public class DbAutoCreator: IDbAutoCreator
+    {
+        [AttributeUsage(AttributeTargets.Class)]
+        public class AutoDBAttribute: Attribute { }
+
+        private readonly string _objWarehousePath;
+        private readonly string _dbDirectory;
+
+        public DbAutoCreator(string objWarehousePath, string dbDirectory)
+        {
+            _objWarehousePath = objWarehousePath;
+            _dbDirectory = dbDirectory;
+        }
+
+        public void GenerateDb()
+        {
+            var assembly = Assembly.LoadFrom(_objWarehousePath);
+
+            if (!Directory.Exists(_dbDirectory))
+            {
+                Directory.CreateDirectory(_dbDirectory);
+            }
+
+            foreach (var type in assembly.GetTypes())
+            {
+                var attribute = type.GetCustomAttribute<AutoDBAttribute>();
+
+                var directoryInfo = new DirectoryInfo(_dbDirectory);
+
+                if (attribute != null && directoryInfo.GetFiles().Where((x) =>
+                x.Name.Contains(type.GetType().Name)).FirstOrDefault() == null)
+                {
+                    File.Create(Path.Combine(_dbDirectory, type.GetType().Name + "Db.json"));
+                }
+            }
+        }
+    }
+}
